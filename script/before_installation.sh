e606599b08fb2f3a003249d61794b0788a4a3482
Bailey-Elson <Baileymitchellelson@gmail.com>
2020-03-16 16:54:47 +0000

    updated

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -4,6 +4,7 @@ sudo apt update -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 sudo apt install python3-venv -y
-sudo apt-get install mysql-server
+sudo apt install guincorn -y
+sudo apt-get install mysql-server -y
 sudo apt-get install libmysqlclient-dev -y
 python3 -m venv venv
--- script/installation.sh
+++ script/installation.sh
@@ -10,12 +10,10 @@ pip3 install flask-bootstrap
 
 pip3 install flask-ckeditor
 
-pip3 install gunicorn
-
 pip3 install Werkzeug
 
 pip3 install Flask-WTF
 
 source ~/.bashrc
 
-python3 app.py
+gunicorn --workers=4 --bind=0.0.0.0:5000 app:app
