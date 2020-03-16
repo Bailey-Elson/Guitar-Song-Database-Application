08e2e996588579875fee674329b553bdfba6f14f
Bailey-Elson <Baileymitchellelson@gmail.com>
2020-03-16 10:17:52 +0000

    Jenkins files

--- script/installation.sh
+++ script/installation.sh
@@ -3,6 +3,6 @@
 source venv/bin/activate
 pip3 install flask
 pip3 install flask_mysqldb
-source ~/.bashrc
-python3 /homea/admin/SFIA1/app.py
+source /home/Admin/bashrc
+python3 /home/Admin/SFIA1/app.py
 
