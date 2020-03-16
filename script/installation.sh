e9a6170ef0b77508ca8c869387b3ca40d544b4cd
Bailey-Elson <Baileymitchellelson@gmail.com>
2020-03-16 11:23:41 +0000

    jenkins

--- script/installation.sh
+++ script/installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-
+  
 source venv/bin/activate
-pip3 install Flask
+pip3 install flask
 pip3 install flask_mysqldb
-source /home/Admin/bashrc
-gunicorn --workers=4 --bind=0.0.0.0:5000 app:app
+source ~/bashrc
+python3 app.py
 
