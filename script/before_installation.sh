306b55b2cb104dd58e8616c6245bcee07e26bbab
Bailey-Elson <Baileymitchellelson@gmail.com>
2020-03-16 13:10:07 +0000

    updated script

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -4,5 +4,6 @@ sudo apt update -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 sudo apt install python3-venv -y
+sudo apt-get install mysql-server
 sudo apt-get install libmysqlclient-dev -y
 python3 -m venv venv
