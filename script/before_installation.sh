8e16ff88bfc7261ac7e602b0a1e212db28fe77b9
Bailey-Elson <Baileymitchellelson@gmail.com>
2020-03-17 11:50:55 +0000

    update

--- script/before_installation.sh
+++ script/before_installation.sh
@@ -4,6 +4,7 @@ sudo apt update -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 sudo apt install python3-venv -y
+sudo apt install python3-pytest -y
 sudo apt install guincorn -y
 sudo apt-get install mysql-server -y
 sudo apt-get install libmysqlclient-dev -y
