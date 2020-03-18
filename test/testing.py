0ace6ac8bd892024bfe0b1029ce00df44af2d539
Bailey-Elson <Baileymitchellelson@gmail.com>
2020-03-18 15:23:04 +0000

    update

--- test/testing.py
+++ test/testing.py
@@ -64,19 +64,90 @@ def test_addChordToSongPage():
     r = http.request('GET', 'http://34.68.124.32:5000/chord/addchordtosong')
     assert 200 == r.status
 
-# app=Flask(__name__)
+app=Flask(__name__)
 
-# app.config['MYSQL_HOST']=os.environ['MYSQLHOST']
-# app.config['MYSQL_USER']=os.environ['MYSQLUSER']
-# app.config['MYSQL_PASSWORD']=os.environ['MYSQLPASSWORD']
-# app.config['MYSQL_DB']=os.environ['MYSQLDB']
+app.config['MYSQL_HOST']=os.environ['MYSQLHOST']
+app.config['MYSQL_USER']=os.environ['MYSQLUSER']
+app.config['MYSQL_PASSWORD']=os.environ['MYSQLPASSWORD']
+app.config['MYSQL_DB']=os.environ['MYSQLDB']
 
-# mysql = MySQL(app)
+mysql = MySQL(app)
 
-# def test_select():
-#     with app.app_context():
-#         cur = mysql.connection.cursor()
-#         numRecords = cur.execte("SELECT * FROM Songs;")
-#         mysql.connection.commit()
-#         cur.close()
-#         assert 19 == numRecords
+def test_createSongsTable():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO Songs(SongID, Song_name, Artist, Genre)VALUES(200, 'Yummy','Justin Bieber','Pop');")
+        numRecords = cur.execute("SELECT * FROM Songs;")
+        mysql.connection.commit()
+        cur.close()
+        assert 20 == numRecords
+
+def test_createChordsTable():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO Chords(ChordID, Chord_name, Chord_symbol)VALUES('200', 'D','Major');")
+        numRecords = cur.execute("SELECT * FROM Chords;")
+        mysql.connection.commit()
+        cur.close()
+        assert 9 == numRecords
+
+def test_createChords_SongsTable():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO Chords_Songs(ID, SongID, ChordID)VALUES('200, '200','200');")
+        numRecords = cur.execute("SELECT * FROM Songs;")
+        mysql.connection.commit()
+        cur.close()
+        assert 52 == numRecords
+
+def test_deleteChords_SongsTable():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM Chords_Songs WHERE ID = '200';")
+        numRecords = cur.execute("SELECT * FROM Songs;")
+        mysql.connection.commit()
+        cur.close()
+        assert 51 == numRecords
+
+def test_deleteSongsTable():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM Songs WHERE SongID = '200';")
+        numRecords = cur.execute("SELECT * FROM Songs;")
+        mysql.connection.commit()
+        cur.close()
+        assert 19 == numRecords        
+
+def test_deleteChordsTable():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        cur.execute("DELETE FROM Chords WHERE ChordID = '200';")
+        numRecords = cur.execute("SELECT * FROM Chords;")
+        mysql.connection.commit()
+        cur.close()
+        assert 8 == numRecords
+
+def test_readSongsTable():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        numRecords = cur.execute("SELECT * FROM Songs;")
+        mysql.connection.commit()
+        cur.close()
+        assert 19 == numRecords
+
+def test_readChordsTable():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        numRecords = cur.execute("SELECT * FROM Chords;")
+        mysql.connection.commit()
+        cur.close()
+        assert 8 == numRecords
+
+def test_readChords_SongsTable():
+    with app.app_context():
+        cur = mysql.connection.cursor()
+        numRecords = cur.execute("SELECT * FROM Chords_Songs;")
+        mysql.connection.commit()
+        cur.close()
+        assert 51 == numRecords
+
